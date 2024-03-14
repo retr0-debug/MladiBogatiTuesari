f8a085b2e1de118e5abc61113a20579994d1f714
Aleks <amd.lambda@gmail.com>
2024-03-14 13:47:42 +0200

    added rectangle icon

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@ var map = L.map('map', {
     scrollWheelZoom: true,
     setZoom: 5, 
   }).setView([54.5, 4.6], 15);
-
+//==========================================================
 const toiletIcon = L.icon({
     iconUrl: 'https://img.icons8.com/ios-filled/50/toilet.png',
     iconSize: [35, 35],
@@ -15,7 +15,7 @@ const marker1 = L.marker([54.7, 2.9], {
 }).addTo(map);
 marker1.bindTooltip("Toilet");
 
-
+//===========================================================
 const teacherRoomicon = L.icon({
     iconUrl: 'https://img.icons8.com/ios-filled/50/meeting-room.png',
     iconSize: [40, 40]
@@ -24,6 +24,31 @@ const teacherRoomicon = L.icon({
 const marker2 = L.marker([54.7, 5.9], {
     icon: teacherRoomicon
 }).addTo(map);
+marker2.bindTooltip("Teacher's Room");
+
+
+//==============================================================
+const pointOfInterest = L.icon({
+    iconUrl: 'https://img.icons8.com/ios-filled/50/point-of-interest.png',
+    iconSize: [40, 40]
+});
+
+const marker3 = L.marker([54.7, 6.9], {
+    icon: pointOfInterest
+}).addTo(map);
+marker3.bindTooltip("Point of interest");
+
+//======================================================================
+//==============================================================
+const rectangle = L.icon({
+    iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
+    iconSize: [40, 40]
+});
+
+const marker4 = L.marker([55.7, 6.9], {
+    icon: rectangle
+}).addTo(map);
+marker4.bindTooltip("rectangle1");
 
 var osm = L.imageOverlay('proekthacktues.png', [[57, -0.99], [50, 10]]);
 osm.addTo(map);
