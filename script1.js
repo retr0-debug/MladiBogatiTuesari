024b29097d58da4420e8fdabca0c62d01e6ad2e1
Aleks <amd.lambda@gmail.com>
2024-03-14 11:46:56 +0200

    fixed toilet icon

--- script1.js
+++ script1.js
@@ -1,17 +1,21 @@
 var map = L.map('map', {
     minZoom: 5,
-    maxZoom: 10,
+    maxZoom: 7,
     scrollWheelZoom: true,
-  }).setView([55, -0.9], 15);
+    setZoom: 5, 
+  }).setView([54.5, 4.6], 15);
 
-const basicBeachIcon = L.icon({
+const toiletIcon = L.icon({
     iconUrl: 'https://img.icons8.com/ios-filled/50/toilet.png',
     iconSize: [40, 40],
 });
 
-const marker1 = L.marker([55, -0.9], {
-    icon: basicBeachIcon
+
+const marker1 = L.marker([54.7, 2.9], {
+    icon: toiletIcon
 }).addTo(map);
+
+marker1.bindTooltip("Toilet");
 
 var osm = L.imageOverlay('proekthacktues.png', [[57, -0.99], [50, 10]]);
 osm.addTo(map);
