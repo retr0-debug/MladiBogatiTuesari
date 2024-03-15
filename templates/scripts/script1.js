73df8c937f2a46f4dc9d7c76fcfdca2f0f9928c1
Aleks <amd.lambda@gmail.com>
2024-03-15 12:15:59 +0200

    updated blok10 map and included some more locations

--- templates/scripts/script1.js
+++ templates/scripts/script1.js
@@ -1,9 +1,8 @@
 var map = L.map('map', {
     minZoom: 6,
     maxZoom: 7,
-    scrollWheelZoom: true,
-    setZoom: 5, 
-  }).setView([54.5, 4.6], 15);
+    scrollWheelZoom: true 
+  }).setView([54.5, 4.6], 6);
 //==========================================================
 const toiletIcon = L.icon({
     iconUrl: 'https://img.icons8.com/ios-filled/50/toilet.png',
@@ -40,8 +39,13 @@ const marker4 = L.marker([55.15, 12.05], {
 }).addTo(map);
 marker4.bindTooltip("9205");
 
-marker4.bindPopup("9205 popup");
+var marker4pop = `<b>9205:</b><br>
+Нещо като зала за лекции.
+Главно за поправки по градивни елементи.
+На почти всеки чин има рисунки от ученици за ученици.
 
+`
+marker4.bindPopup(marker4pop);
 
 //=====================================================================
 const rectangle2 = L.icon({
@@ -54,6 +58,15 @@ const marker5 = L.marker([53.53, 13.31], {
 }).addTo(map);
 marker5.bindTooltip("9230");
 
+var marker5pop = `<b>9230:</b><br>
+Малка зелена стая.
+Лявата редица е хубава, дясната не става.
+Вратата по някаква причина е най-отзад на стаята.
+Floor loot: JPG Le Beau, пречиствател на въздух, туба.
+Еднo момче се напи и припадна в нея.
+`
+marker5.bindPopup(marker5pop);
+
 //===========================================================================
 const rectangle3 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -64,6 +77,13 @@ const marker6 = L.marker([51.33, 14.20], {
     icon: rectangle3
 }).addTo(map);
 marker6.bindTooltip("9221");
+
+var marker6pop = `
+<b>9221:</b><br> 
+Голяма е и само това.
+`
+
+marker6.bindPopup(marker6pop);
 //===========================================================================
 const rectangle4 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -74,6 +94,12 @@ const marker7 = L.marker([51.33, 12.35], {
     icon: rectangle4
 }).addTo(map);
 marker7.bindTooltip("9223");
+
+var marker7pop = `<b>9223:</b><br> 
+Голяма е.
+Като цяло става. 
+`
+
 //=========================================================================
 const rectangle5 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -84,16 +110,15 @@ const marker8 = L.marker([52.74 , 14.8], {
     icon: rectangle5
 }).addTo(map);
 marker8.bindTooltip("9217");
-//=======================================================================
-const rectangle6 = L.icon({
-    iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
-    iconSize: [43, 100]
-});
 
-const marker9 = L.marker([52.74 , 14.8], {
-    icon: rectangle6
-}).addTo(map);
-marker9.bindTooltip("9217");
+var marker8pop = `
+<b>9217:</b><br> 
+Много е зле.
+Поне е слънчево.
+`
+marker8.bindPopup(marker8pop);
+
+
 //===========================================================================
 const rectangle7 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -104,6 +129,16 @@ const marker10 = L.marker([54.53 , 9.87], {
     icon: rectangle7
 }).addTo(map);
 marker10.bindTooltip("9224");
+
+var marker10pop = `<b>9224:</b><br>
+Компютърна зала.
+Има 14 лаптопа и може би има 14 мишки и зарядни.
+Мила Нейкова hangout spot.
+Понякога има адекватни учители. 
+Има мултимедия и в хубав ден ще работи.
+`
+
+marker10.bindPopup(marker10pop);
 //=============================================================================
 const rectangle8 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -114,6 +149,13 @@ const marker11 = L.marker([54.53 , 7.87], {
     icon: rectangle8
 }).addTo(map);
 marker11.bindTooltip("9222");
+
+var marker11pop = `
+<b>9222:</b><br>
+Компютърна зала.
+Има около 14 лаптопа, но няма 14 мишки и зарядни.
+`
+marker11.bindPopup(marker11pop);
 //==========================================================================
 const rectangle9 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -124,6 +166,12 @@ const marker12 = L.marker([54.53 , 6.25], {
     icon: rectangle9
 }).addTo(map);
 marker12.bindTooltip("9218");
+
+var marker12pop = `<b>9218:</b><br> 
+Не става.
+`
+
+marker12.bindPopup(marker12pop);
 //============================================================================
 const rectangle10 = L.icon({
     iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
@@ -135,6 +183,63 @@ const marker13 = L.marker([54.53 , 0.94], {
 }).addTo(map);
 marker13.bindTooltip("9202");
 
+var marker13pop = `<b>9202</b><br>
+Голяма стая.
+Зелена е.
+Точно до учителската стая, за това не прави глупости.
+`
+marker13.bindPopup(marker13pop);
+
+//===============================================================================
+const pointOfInterest = L.icon({
+    iconUrl: 'https://img.icons8.com/ios-filled/50/point-of-interest.png',
+    iconSize: [40, 40]
+});
+
+const marker14 = L.marker([55.2, 13.3], {
+    icon: pointOfInterest
+}).addTo(map);
+marker14.bindTooltip("Point of interest");
+
+var marker14pop = `<b>Random стълби в коридора:</b><br>
+Защо съществуват?
+`
+
+marker14.bindPopup(marker14pop);
+
+//==========================================================================
+const pointOfInterest2 = L.icon({
+    iconUrl: 'https://img.icons8.com/ios-filled/50/point-of-interest.png',
+    iconSize: [40, 40]
+});
+
+const marker15 = L.marker([53.27, 14.8], {
+    icon: pointOfInterest2
+}).addTo(map);
+marker15.bindTooltip("Point of interest");
+
+var marker15pop = `<b>Огледало на стълбите:</b><br>
+Едно от малкото огледала в сградата.
+
+`
+marker15.bindPopup(marker15pop);
+
+//===========================================================
+const pointOfInterest3 = L.icon({
+    iconUrl: 'https://img.icons8.com/ios-filled/50/point-of-interest.png',
+    iconSize: [40, 40]
+});
+
+const marker16 = L.marker([55.25, 3.4], {
+    icon: pointOfInterest3
+}).addTo(map);
+marker16.bindTooltip("Point of interest");
+
+var marker16pop = `<b>Асансьор:</b><br>
+Никой не знае къде води.
+Може би не работи.
+`
+marker16.bindPopup(marker16pop);
 
 
 
--- templates/scripts/script10.js
+++ templates/scripts/script10.js
@@ -2,8 +2,7 @@ var map = L.map('map', {
     minZoom: 6,
     maxZoom: 7,
     scrollWheelZoom: true,
-    setZoom: 5, 
-  }).setView([54.5, 4.6], 15);
+  }).setView([54.5, 4.6], 6);
 
 
 //=========================================================================
@@ -12,11 +11,34 @@ const rectangle5 = L.icon({
     iconSize: [67, 169]
 });
 
-const marker8 = L.marker([54.09 , 4.7], {
+const marker8 = L.marker([54.09 , 4], {
     icon: rectangle5
 }).addTo(map);
 marker8.bindTooltip("Книжарница");
 
+var marker8pop = `<b>Книжарница:</b><br>
+Главно за хартия за контролни(при г-жа Абрамович) и калкулатори(при г-жа Башева).
+До нея има античен телефон.
+`
+marker8.bindPopup(marker8pop);
+
+//==========================================================================
+const rectangle6 = L.icon({
+    iconUrl: 'https://img.icons8.com/pulsar-line/48/rounded-rectangle.png',
+    iconSize: [60, 85]
+});
+
+const marker9 = L.marker([53.79 , 9.34], {
+    icon: rectangle6
+}).addTo(map);
+marker9.bindTooltip("Vending машини");
+
+var marker9pop = `<b>Вендинг машина:</b><br>
+Legendary location.
+Инфлацията е <em>insane</em>.
+Има шанс да получиш 2 вафли „Боровец“.
+`
+marker9.bindPopup(marker9pop);
 
 var osm = L.imageOverlay('10blok_layout.png', [[57, -0.99], [50, 16]]);
 osm.addTo(map);
