c2e9c9174889183c7d018672e08f8a25c3066e3c
nikolp18 <npeneva08@gmail.com>
2024-04-16 22:35:24 +0300

    login and sign up

--- app.py
+++ app.py
@@ -1,65 +1,138 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import sqlite3
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, static_url_path='/static')
-app.secret_key = 'your_secret_key'
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError('This username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
 
 @app.route('/')
-@app.route('/home')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/home',  methods=['GET', 'POST'])
+@login_required
 def index():
     return render_template('index.html')
 
 @app.route("/block8")
+@login_required
 def block8_floors():
     return render_template("etazhiza8.html")
 
 @app.route("/block8_2")
+@login_required
 def block8_1():
     return render_template("block8_2.html")
 
 @app.route("/block8_3")
+@login_required
 def block8_3():
     return render_template("block8_3.html")
 
 @app.route("/block8_4")
+@login_required
 def block8_4():
     return render_template("block8_4.html")
 
 @app.route("/block9")
+@login_required
 def block9_floors():
     return render_template("etazhiza9.html")
 
 @app.route("/block9_1")
+@login_required
 def block9_1():
     return render_template("block9_1.html")
 
 @app.route("/block9_2")
+@login_required
 def block9_2():
     return render_template("block9.html")
 
 @app.route("/block10")
+@login_required
 def block10():
     return render_template("block10.html")
 
 @app.route("/daskalo")
+@login_required
 def daskalo():
     return render_template("daskalo.html")
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-@app.route('/login')
-def login():
-    return render_template('base.html', content='login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('base.html', content='signup.html')
-
-@app.route('/unauthorized')
-def unauthorized():
-    return render_template('base.html', content='unauthorized.html')
-
-#if __name__ == '__main__':
-    #app.run(debug=True)
-
